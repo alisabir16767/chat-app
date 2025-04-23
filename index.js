880e52a3b52cae901053e85b6f552b41e6ddace8
Sabir Ali <alisabir167167@gmail.com>
2025-04-23 15:22:56 +0530

    socket completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = requre("express");
-const app = express();
-const port = 3000;
+const express = require("express");
+const { createServer } = require("http");
+const { join } = require("path");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Serve the HTML file
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// Socket.IO connection event
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
